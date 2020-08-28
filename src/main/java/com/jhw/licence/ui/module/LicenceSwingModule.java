25a924d2782aa8b02c0fa1471d98cb1749093647
Unknown <unknown@example.com>
2020-08-27 20:05:07 -0500

    SwingModule: extends

--- src/main/java/com/jhw/licence/ui/module/LicenceSwingModule.java
+++ src/main/java/com/jhw/licence/ui/module/LicenceSwingModule.java
@@ -5,7 +5,7 @@ import com.clean.core.app.services.Notification;
 import com.clean.core.app.services.NotificationsGeneralType;
 import com.clean.core.domain.services.Resource;
 import com.clean.swing.app.AbstractSwingApplication;
-import com.clean.swing.app.AbstractSwingMainModule;
+import com.clean.swing.app.DefaultAbstractSwingMainModule;
 import com.clean.swing.app.dashboard.DashBoardSimple;
 import com.clean.swing.app.dashboard.DashboardConstants;
 import com.jhw.licence.core.module.LicenceCoreModule;
@@ -20,7 +20,7 @@ import java.awt.event.ActionEvent;
 import java.net.MalformedURLException;
 import javax.swing.AbstractAction;
 
-public class LicenceSwingModule implements AbstractSwingMainModule {
+public class LicenceSwingModule extends DefaultAbstractSwingMainModule {
 
     public static final String MSG_DAYS_TO_ACTIVATE = "msg.licence.days_to_activate";
 
@@ -70,10 +70,6 @@ public class LicenceSwingModule implements AbstractSwingMainModule {
     @Override
     public void navigateTo(String string, Object... o) {
         navigator.navigateTo(string, o);
-    }
-
-    @Override
-    public void closeModule() {
     }
 
 }
