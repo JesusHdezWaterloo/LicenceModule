22706564058579711bc1ff12e49ee207b7b569f6
Unknown <unknown@example.com>
2020-12-26 12:08:15 -0500

    LicenceConververSecured: *ALL :+1:

--- src/main/java/com/jhw/module/util/licence/repo/repo_impl/LicenceConververSecured.java
+++ src/main/java/com/jhw/module/util/licence/repo/repo_impl/LicenceConververSecured.java
@@ -7,8 +7,10 @@ package com.jhw.module.util.licence.repo.repo_impl;
 
 import com.clean.core.app.repo.Converter;
 import com.jhw.module.util.licence.core.domain.LicenceDomain;
-import com.jhw.module.util.licence.core.domain.LicenceDomainSimpleConverter;
 import com.jhw.module.util.licence.repo.entity.Licence;
+import com.jhw.utils.security.AES;
+import com.jhw.utils.services.ConverterService;
+import java.util.Base64;
 
 /**
  *
@@ -16,20 +18,43 @@ import com.jhw.module.util.licence.repo.entity.Licence;
  */
 public class LicenceConververSecured implements Converter<LicenceDomain, Licence> {
 
+    public static final byte[] HARDCORE_PASS = new byte[0];
+
     @Override
     public LicenceDomain from(Licence entity) throws Exception {
         //el string de la BD cifrado y en base 64
         String string = entity.getLicence();
-        //decifrado
-        return LicenceDomainSimpleConverter.getInstance().from(string);
+        string = decipher(string);
+        return ConverterService.convert(string, LicenceDomain.class);
     }
 
     @Override
     public Licence to(LicenceDomain object) throws Exception {
         //el objeto limpio, cifrarlo y encodearlo e 64
-        String string = LicenceDomainSimpleConverter.getInstance().to(object);
-        //cifrado
+        String string = ConverterService.convert(object, String.class);
+        string = cipher(string);
         return new Licence("cod_cliente_UNKNOWN", string);
     }
 
+    private String cipher(String text) throws Exception {
+        return new String(
+                Base64.getEncoder().encode(
+                        AES.cipher(
+                                HARDCORE_PASS,
+                                text.getBytes()
+                        )
+                )
+        );
+    }
+
+    private String decipher(String text) throws Exception {
+        return new String(
+                AES.decipher(
+                        HARDCORE_PASS, 
+                        Base64.getDecoder().decode(
+                                text
+                        )
+                )
+        );
+    }
 }
