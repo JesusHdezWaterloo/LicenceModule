0c8a90174ba0ca1bf4b7c89b55ccd924acfae8b0
Unknown <unknown@example.com>
2020-12-26 12:08:58 -0500

    LicenceRepoImpl: -activate

--- src/main/java/com/jhw/module/util/licence/repo/repo_impl/LicenceRepoImpl.java
+++ src/main/java/com/jhw/module/util/licence/repo/repo_impl/LicenceRepoImpl.java
@@ -13,17 +13,6 @@ public class LicenceRepoImpl extends JPACleanCRUDRepo<LicenceDomain, Licence> im
         this.setConverter(new LicenceConververSecured());
     }
 
-    // El String de licence viene cifrado y en Base64
-    @Override
-    public LicenceDomain activate(String licence) throws Exception {
-        Licence entity = new Licence("no_code", licence);
-        LicenceDomain domain = converter.from(entity);
-        if (!domain.checkIntegrity()) {
-            throw new NullPointerException("Error activando el programa.");
-        }
-        return domain;
-    }
-
     @Override
     public LicenceDomain read() throws Exception {
         return findAll().get(0);
