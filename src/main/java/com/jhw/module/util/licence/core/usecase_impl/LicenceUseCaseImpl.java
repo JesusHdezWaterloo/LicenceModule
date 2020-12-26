431063890f05a9b0af91aca684736f6200c05839
Unknown <unknown@example.com>
2020-12-26 12:03:03 -0500

    LicenceUseCaseImpl: *activate

--- src/main/java/com/jhw/module/util/licence/core/usecase_impl/LicenceUseCaseImpl.java
+++ src/main/java/com/jhw/module/util/licence/core/usecase_impl/LicenceUseCaseImpl.java
@@ -4,12 +4,11 @@ import com.clean.core.app.services.ExceptionHandler;
 import com.clean.core.app.usecase.DefaultCRUDUseCase;
 import com.clean.core.domain.services.Resource;
 import com.jhw.module.util.licence.core.repo_def.LicenceRepo;
-import com.google.common.io.BaseEncoding;
 import com.jhw.module.util.licence.core.usecase_def.LicenceUseCase;
 import com.jhw.module.util.licence.core.domain.LicenceDomain;
+import com.jhw.module.util.licence.core.domain.LicenceDomainSimpleConverter;
 import com.jhw.module.util.licence.core.module.LicenceCoreModule;
 import com.jhw.module.util.licence.core.exception.BadLicenceException;
-import com.jhw.utils.security.AES;
 import java.time.LocalDate;
 
 /**
@@ -19,8 +18,6 @@ import java.time.LocalDate;
  * @author Jesus Hernandez Barrios (jhernandezb96@gmail.com)
  */
 public class LicenceUseCaseImpl extends DefaultCRUDUseCase<LicenceDomain> implements LicenceUseCase {
-
-    public static final byte[] HARDCORE_PASSWORD = new byte[]{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};//SerialNumber.getUID();
 
     public static final String MSG_NO_FILE = "msg.licence.no_file";
     public static final String MSG_INVALID = "msg.licence.invalid";
@@ -90,12 +87,16 @@ public class LicenceUseCaseImpl extends DefaultCRUDUseCase<LicenceDomain> implem
      * Activa la licencia. Si el codigo de cifrado esta bien, la activa y guarda
      * en memoria, sino lanza excepcion.
      *
-     * @param codeCypher codigo de activacion cifrado
+     * @param actvationCode codigo de activacion cifrado
      * @throws Exception si hay algun problema en la activacion
      */
     @Override
-    public void activateLicence(String codeCypher) throws Exception {
-        repo.activate(codeCypher);
+    public void activate(String actvationCode) throws Exception {
+        LicenceDomain domain = LicenceDomainSimpleConverter.getInstance().from(actvationCode);
+        if (!domain.checkIntegrity()) {
+            throw new NullPointerException("Error activando el programa.");
+        }
+        write(domain);
     }
 
     @Override
