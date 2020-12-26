46cefb97306c598b3ac40fa1e7e1ddee479d5c6e
Unknown <unknown@example.com>
2020-12-26 12:03:11 -0500

    Main: test

--- src/main/java/com/jhw/module/util/licence/Main.java
+++ src/main/java/com/jhw/module/util/licence/Main.java
@@ -7,9 +7,15 @@ package com.jhw.module.util.licence;
 
 import com.jhw.module.util.licence.core.usecase_def.LicenceUseCase;
 import com.jhw.module.util.licence.core.domain.LicenceDomain;
+import com.jhw.module.util.licence.core.domain.LicenceDomainSimpleConverter;
 import com.jhw.module.util.licence.core.module.LicenceCoreModule;
 import com.jhw.module.util.licence.generator.GENERATOR;
+import com.jhw.module.util.licence.repo.entity.Licence;
 import com.jhw.module.util.licence.repo.module.LicenceRepoModule;
+import com.jhw.module.util.licence.rest.A_ModuleUtilLicence;
+import com.jhw.module.util.mysql.services.MySQLHandler;
+import com.jhw.utils.services.ConverterService;
+import java.time.LocalDate;
 import java.util.Date;
 
 /**
@@ -22,24 +28,34 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) throws Exception {
-        /*
-        LicenceCoreModule.init(LicenceRepoModule.init());
-
-        System.out.println("123");
-        Date inicio = new Date();
-        Date fin = new Date(150, 7, 21);
-        long token = GENERATOR.generateToken(inicio, fin, CONFIG.DIFICULTY);
-        LicenceDomain lic = new LicenceDomain(token, inicio, fin);
-
-        String TO_SEND = GENERATOR.generateActivationCode(lic, CONFIG.HARDCORE_PASSWORD);
-        System.out.println(TO_SEND);
-        LicenceUseCase useCase = LicenceCoreModule.getInstance().getImplementation(LicenceUseCase.class);
-        useCase.activateLicence(TO_SEND);
-        System.out.println(useCase.isLicenceCorrect());
-
-        //LicenceUseCase useCase = new LicenceUseCaseImpl(new File("licence.lic"));
-        //System.out.println(useCase.isLicenceCorrect());
-        */
+        //create();
+        read();
+        //convert();
     }
 
+    private static void create() throws Exception {
+        LocalDate inicio = LocalDate.now();
+        LocalDate fin = inicio.plusDays(30);
+        long token = 7848;//GENERATOR.generateToken(inicio, fin, DIFICULTY.VALUE);
+
+        LicenceDomain lic = new LicenceDomain(token, inicio, fin);
+
+        //String TO_SEND = GENERATOR.generateActivationCode(lic, new byte[0]);
+        String TO_SEND = LicenceDomainSimpleConverter.getInstance().to(lic);
+        A_ModuleUtilLicence.licenceUC.activate(TO_SEND);
+    }
+
+    private static void read() throws Exception {
+        LicenceDomain lic = A_ModuleUtilLicence.licenceUC.read();
+        System.out.println(lic);
+    }
+
+    private static void convert() throws Exception {
+        LicenceDomain l = new LicenceDomain(7489, LocalDate.now(), LocalDate.now().plusDays(50));
+        String toString = ConverterService.convert(l, String.class);
+        System.out.println(toString);
+
+        LicenceDomain l2 = ConverterService.convert(toString, LicenceDomain.class);
+        System.out.println(l2);
+    }
 }
